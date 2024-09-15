639f23ea331baafd95c34d60b7105b2cfd0114a1
seratlizzeynep <zeynepaybuke.seratli@tedu.edu.tr>
2024-09-15 21:41:46 +0300

    Add files via upload

---
+++ mmm.java
@@ -0,0 +1,21 @@
+package deneme;
+
+public class mmm {
+
+	    public static void main(String[] args) {
+	       
+
+	        // Her bir satır için döngü
+	        for (int i = 1; i < 8; i+=2) {
+	            
+	            
+	            // Yıldızları yazdırmak için döngü
+	            for (int k = 0; k <= i-1; k++) {
+	                System.out.print("*");
+	            }
+	            
+	            
+	            System.out.println();
+	        }
+	    }
+	}
