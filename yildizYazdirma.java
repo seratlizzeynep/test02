39d375de677566ea36332cecc1c0096a08f5536a
seratlizzeynep <zeynepaybuke.seratli@tedu.edu.tr>
2024-09-15 21:42:46 +0300

    Update and rename mmm.java to yildizYazdirma.java

--- yildizYazdirma.java
+++ yildizYazdirma.java
@@ -1,15 +1,14 @@
 package deneme;
 
-public class mmm {
+public class yildizYazdirma {
 
 	    public static void main(String[] args) {
 	       
 
-	        // Her bir satır için döngü
 	        for (int i = 1; i < 8; i+=2) {
 	            
 	            
-	            // Yıldızları yazdırmak için döngü
+	         
 	            for (int k = 0; k <= i-1; k++) {
 	                System.out.print("*");
 	            }
