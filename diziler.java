af18d7e20c03487aa84be74adb7e4f01b492d136
seratlizzeynep <zeynepaybuke.seratli@tedu.edu.tr>
2024-09-16 19:53:40 +0300

    Add files via upload

---
+++ diziler.java
@@ -0,0 +1,17 @@
+package github;
+
+public class diziler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String harfnotlari [] = {"AA","BA","BB","BC","CB","CC","DC","DD"};
+		
+		for (int i = 0; i < harfnotlari.length; i++) {
+			
+			System.out.println(harfnotlari[i]);
+		}
+				
+	}
+
+}
