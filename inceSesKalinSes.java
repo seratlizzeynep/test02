cf7ae1bc64b691cfd1d649957d686302d84b471a
seratlizzeynep <zeynepaybuke.seratli@tedu.edu.tr>
2024-09-17 22:44:22 +0300

    Add files via upload

---
+++ inceSesKalinSes.java
@@ -0,0 +1,23 @@
+package github;
+
+public class inceSesKalinSes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char harf ='E';
+		switch (harf) {
+		case 'A':
+		case 'I':
+		case 'O':
+		case 'U':
+			
+		System.out.println("Kalin sesli harf");
+		break;
+		default:
+			
+		System.out.println("İnce sesli harf");
+		}
+	}
+
+}
